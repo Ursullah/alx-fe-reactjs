2e4f0a541bda2ef76b11e23ebe28045f7ca3d2f8
Ursullah <khanguhaursula@gmail.com>
2025-02-26 13:59:46 +0300

    Added NavBra file

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
+      <ul style={{ display: 'flex', gap: '15px', listStyle: 'none' }}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;

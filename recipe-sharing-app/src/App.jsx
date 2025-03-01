b447fa867bbce2d59614fc54b5eb762d9b4a8180
Ursullah <khanguhaursula@gmail.com>
2025-03-01 15:42:28 +0300

    Moved and updated recipe store

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import RecipeList from "./components/RecipeList"
 import AddRecipeForm from "./components/AddRecipeForm"
 
+
 const App = () => {
   return (
     <>
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
   // RecipeList component
-  import  useRecipeStore  from '../store/recipeStore.js';
+  import  useRecipeStore  from './recipeStore.js';
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);

e722eb838fc714f53b903e622d10097a016e8717
Ursullah <khanguhaursula@gmail.com>
2025-03-01 15:36:28 +0300

    Recipe sharing app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RecipeList from "./components/RecipeList"
+import AddRecipeForm from "./components/AddRecipeForm"
+
+const App = () => {
+  return (
+    <>
+ <RecipeList />
+ <AddRecipeForm />
+ </>
+  )
+}
+
+export default App;

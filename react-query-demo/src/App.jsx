4a02cd9f03c98ca987282f1e3c91de81b5c21608
Ursullah <khanguhaursula@gmail.com>
2025-03-06 14:57:31 +0300

    Updated post component

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import MyComponent from './components/MyComponents.jsx'
-import PostComponent from './components/PostComponent.jsx';
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
+import PostComponent from './components/PostComponents.jsx'
 
 const queryClient = new QueryClient();
 
 function App(){
   return(
     <QueryClientProvider client ={queryClient}>
-    <MyComponent />
+    <div>
+      <h1>Posts</h1>
     <PostComponent />
+    </div>
     </QueryClientProvider>
   );
-}
+};
 
 export default App

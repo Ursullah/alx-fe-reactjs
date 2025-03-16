2953dcc271dc086746f0b4c4238d0a6e591e8d57
Ursullah <khanguhaursula@gmail.com>
2025-03-16 20:14:38 +0300

    Responsiveness updated

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 p-8 md:p-8 mx-auto my-20 rounded-lg shadow-lg max-w-xs md:max-w-sm sm:p-4">
       <img src="https://via.placeholder.com/150" className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36" alt="User" />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:-xl">John Doe</h1>
+      <h1 className="text-xl text-blue-800 p-8 md:p-8 my-4 sm:text-lg md:-xl">John Doe</h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );

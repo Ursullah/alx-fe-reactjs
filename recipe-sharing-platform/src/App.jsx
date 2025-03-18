68e1cddf24097f838b879aa582cb279d4ecdbd25
Ursullah <khanguhaursula@gmail.com>
2025-03-18 18:31:58 +0300

    Added styles to the images

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -5,9 +5,6 @@ const App = () => {
   return (
     <>
     <div>
-       <h1 class="text-3xl font-bold underline">
-       Hello world!
-       </h1>
        <HomePage />
     </div>
     </>
--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,38 +1,36 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import data from "../data.json";
 
-
 const HomePage = () => {
-    const [recipes,setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
-    useEffect(() => { 
-        setRecipes(data);},
-        []);
-    //     fetch('../data.json')
-    //     .then(response =>{
-    //         if(!response.ok){
-    //             throw new Error ('Nework response was not ok')
-    //         }
-    //         return response.json();
-    //     })
-    //     .then (data=>setRecipes(data))
-    //     .catch(error=>console.error('Fetch error:', error));
-    // }, [])
+  useEffect(() => {
+    setRecipes(data);
+  }, []);
+
   return (
-    <div>
-       <h1>Recipe List</h1>
-       <div>
-        {recipes.map((recipe)=>(
-            <div key={recipe.id}>
-                <h2>{recipe.title}</h2>
-                <p>{recipe.summary}</p>
+    <div className="p-6 bg-gray-400 min-h-screen">
+      <h1 className="text-3xl font-bold text-center mb-6">Recipe List</h1>
+      <div>
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="bg-white rounded-lg shadow-md p-4">
+            <h2 className="text-xl font-semibold mt-2">{recipe.title}</h2>
 
+            {/* Image Wrapper to Ensure Proper Sizing */}
+            <div>
+              <img
+                  className="min-w-25 min-h-24 rounded-md "
+                src={recipe.image}
+                alt={recipe.title}
+              />
             </div>
+
+            <p className="text-gray-600 mt-1">{recipe.summary}</p>
+          </div>
         ))}
-       </div>
+      </div>
     </div>
-  )
+  );
 };
 
-export default HomePage
+export default HomePage;

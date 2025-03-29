9023f9ee88d72eacec4d2aafe53a5f464c032130
Ursullah <khanguhaursula@gmail.com>
2025-03-29 20:06:33 +0300

    Error message included in catch

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -25,7 +25,7 @@ function Search() {
         }
       }
     } catch (err) {
-      setError("Looks like we can't find the user.");
+      setError("Looks like we can't find the user");
     } finally {
       setLoading(false);
     }

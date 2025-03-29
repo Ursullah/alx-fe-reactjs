559980e59a97c7cde0ff72f7c90292db546b6a04
Ursullah <khanguhaursula@gmail.com>
2025-03-29 19:48:38 +0300

    Used axios for fetching from API

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,15 @@
+import axios from "axios";
+
 const API_URL = "https://api.github.com/search/users";
 const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
 const fetchGitHubUsers = async (query, page = 1) => {
   try {
-    const response = await fetch(`${API_URL}?q=${query}&page=${page}`, {
+    const response = await axios.get(API_URL, {
+      params: { q: query, page },
       headers: { Authorization: `token ${API_KEY}` }
     });
-    const data = await response.json();
-    return data.items || [];
+    return response.data.items || [];
   } catch (error) {
     console.error("Error fetching GitHub users:", error);
     return [];
